51515ba89ee2d11652270d3edd58aa331f79338d
ean <emyrade29@gmail.com>
2021-04-07 02:04:19 -0400

    dontate-modal-and in views and buttones style

--- CrowdFunding/CrowdFunding/settings.py
+++ CrowdFunding/CrowdFunding/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_project',
-        'USER': 'mamdouh',
-        'PASSWORD': '123',
+        'USER': 'Eman',
+        'PASSWORD': 'emy1921998',
         'HOST': '127.0.0.1',
     }
 }
--- CrowdFunding/projects/urls.py
+++ CrowdFunding/projects/urls.py
@@ -1,23 +1,22 @@
 
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',  views.index , name='projects'),
+    path('',  views.index, name='projects'),
     path('create', views.Create, name='newProject'),
-    path('view/<int:project_id>',views.show, name='viewProject'),
-    path('edit/<int:project_id>',views.update, name='editProject'),
-    path('new_rate/<int:projectId>',views.new_rate, name='new_rate'),
-    path('project_rate/<int:projectId>',views.rate_project, name='project_rate'),
-    path('new_donate.html/<int:projectId>',views.new_donate, name='new_donate'),
-    path('delete/<int:project_id>',views.deleteProject, name='deleteProject'),
+    path('view/<int:project_id>', views.show, name='viewProject'),
+    path('edit/<int:project_id>', views.update, name='editProject'),
+    # path('new_rate/<int:projectId>', views.new_rate, name='new_rate'),
+    path('project_rate/<int:projectId>',
+         views.rate_project, name='project_rate'),
+    # path('new_donate.html/<int:projectId>',views.new_donate, name='new_donate'),
+    path('delete/<int:project_id>', views.deleteProject, name='deleteProject'),
 
-    
-   
-    
+
+
+
 ]
-
-
