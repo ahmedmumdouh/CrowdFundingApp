33f22e3e31bba0eb0e5040d76ae2adb71b795b12
ahmedmumdouh <ahmedmumdouh@gamil.com>
2021-04-08 00:25:02 +0200

    auth pass bouns confirm + authenticate over all pages

--- CrowdFunding/home/urls.py
+++ CrowdFunding/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path , include
 from . import views
 
 
+
 urlpatterns = [
     path('',  views.index , name='index'),
     path('new_category',  views.new_category , name='new_category'),
