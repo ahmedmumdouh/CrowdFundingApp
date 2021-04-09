9190491df5912b3ac58d9e364997ca04fe2cbcc4
ahmedmumdouh <ahmedmumdouh@gamil.com>
2021-04-09 02:05:05 +0200

    Add(feat-dev-home): user-auth-resetPass + changePass + category style

--- CrowdFunding/CrowdFunding/urls.py
+++ CrowdFunding/CrowdFunding/urls.py
@@ -26,19 +26,33 @@ from django.conf.urls.static import static
 from . import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls , name='admin'),
     path('', include('home.urls')),
-    path('puser', include('pusers.urls')),
+    path('puser/', include('pusers.urls')),
     path('projects/', include('projects.urls')),
     path('comments/', include('comments.urls')),
-    
     path('reports/', include('reports.urls')),
     
     
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='pusers/password_change_done.html'), 
+        name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='pusers/password_change.html'), 
+        name='password_change'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='pusers/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='pusers/password_reset_complete.html'),
+     name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
