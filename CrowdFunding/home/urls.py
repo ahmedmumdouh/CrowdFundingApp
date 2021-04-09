864dfc05a8b550fcaeb35a0bcf72a9a4035a2694
ahmedmumdouh <ahmedmumdouh@gamil.com>
2021-04-09 23:47:08 +0200

    merge rate + search

--- CrowdFunding/home/urls.py
+++ CrowdFunding/home/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('my_donate',  views.my_donate , name='my_donate'),
     path('show',  views.show, name='show'),
     path('searchName', views.searchName, name='searchName'),
-    path('searchTag', views.searchTag, name='searchTag'),
+   
     path('delete/<int:category_id>',  views.delete_category , name='delete_category'),
 
 ]
